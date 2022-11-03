6018c7b12e6c1d0247d27556647d89dd4b3820a8
kaashvi <kaashvi@BYOD-NISHANT-OT>
2022-11-03 10:06:38 +0400

    repository

---
+++ mavenproject/src/test/java/mavenprojectday1/arraylistnew.java
@@ -0,0 +1,29 @@
+package mavenprojectday1;
+
+import java.util.ArrayList;
+
+public class arraylistnew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList al=new ArrayList();
+		al.add("richa");
+		al.add(100);
+		System.out.println("No of elements"+ al.size());
+		System.out.println("No of elements"+ al);
+		al.add(1,"Kaashvi");
+		System.out.println("No of elements"+ al);
+		//System.out.println("No of elements"+ al);
+		//al.remove(1);
+		
+		System.out.println("No of elements"+ al.size());
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
